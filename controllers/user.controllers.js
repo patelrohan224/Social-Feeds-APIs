b29b43059be41bd04d23f3b2b5a8d15d3e9a3596
Patel Rohan <rohan_fw11_289@masai.school>
2022-01-01 18:13:18 +0000

    t1

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,11 +1,14 @@
 const Users= require('../models/Users.model')
+const nanoid=require('nanoid')
 const getUserByname=async(req, res, next)=>{
+    console.log(req.params);
     try{
         const users=await Users.findOne({username: req.params.username})
         if(!users){
             return res.status(404).json({});
         }
-        return res.status(500).json(users);
+        console.log(users);
+        return res.status(200).json(users);
     }
     catch(err){
         return res.status(404).json({
@@ -14,31 +17,90 @@ const getUserByname=async(req, res, next)=>{
     }
 }
 const createuser= async(req, res, next)=>{
-    try {
-        const users=await Users.find({username:req.body.username})
-        if(users){
-            return res.status(400).json({
-                "status":"failure",
-                "reason":"explanation"
-            })
-        }
-            const user=await Users.create({username: req.body.username})
-            if(user){
-                return res.status(201).json({
-                    username: user.username
-                })
-            }
-            else{
-                return res.status(400).json({
-                    "status":"failure",
-                    "reason":"explanation"
-                })
-            }
-    } catch (error) {
+    console.log(req.body.username);
+    // console.log("here in post ")
+    const temp=await Users.findOne({username:req.body.username});
+
+    if(temp)
+    {
         return res.status(400).json({
-            "status":"failure",
-            "reason":"explanation"
+            status:"failure",
+            reason:"user already exist"
         })
     }
+    const data=await Users.create(req.body);
+    if(data)
+    {
+        // console.log("data added");
+        return res.status(201).json({"username":data.username});
+    }
+    else
+    {
+         return res.status(400).json({
+            status:"failure",
+            reason:"something went wrong"
+        })
+    }
+
 }
-module.exports={getUserByname,createuser}
+const follw= async(req, res, next)=>{
+    const a=await Users.findOne({username:req.params.usernameA});
+    const b=await Users.findOne({username:req.params.usernameB});
+    console.log(a.username,b.username);
+   
+    if(a && b){
+        const d=await Users.findByIdAndUpdate(b._id,{ $push: {followers:a.username}})
+        const e=await Users.findByIdAndUpdate(a._id,{ $push: {following:b.username}})
+        return res.status(202).json({"status": "success"})
+    }
+    
+return res.status(400).json({status:"failure",reason:"user already exist"})
+}
+
+const posst=async(req, res, next)=>{
+    const a=await Users.findOne({username:req.params.username})
+    if(a){
+        const d=await Users.findByIdAndUpdate(a._id,
+            { $push: 
+                {posts:
+                    {imageUrl:req.body.imageUrl,caption:req.body.caption,postId:Math.floor(Math.random() * 10),upvotes:0}
+                }
+            },{new:true}
+            )
+        if(d.posts.length>1){
+            return res.status(201).json(d.posts[d.posts.length-1])            
+        }
+        return res.status(201).json(d.posts[0])
+    }
+        
+return res.status(400).json({status:"failure",reason:"user already exist"})
+
+
+}
+
+const getaa=async(req, res, next)=>{
+    const a=await Users.findOne({username:req.params.usernameA})
+    console.log(a);
+    const posts=[]
+    if(a){
+        const foll=a.following
+        console.log(foll);
+        for (let i = 0; i < foll.length; i++) {
+            const user=await Users.findOne(foll[i]._id)
+            if(user.posts.length>1){
+                for (let j = 0; j < user.posts.length; j++) {
+                    const postUser = user.posts[j]
+                        posts.push(postUser)
+                }
+            }
+            posts.push(user.posts[0])
+        }
+        if(posts.length==0){
+            return res.status(200).json([])
+        }
+        
+        return res.status(200).json(posts)
+    }
+    return res.status(400).json({status:"failure",reason:"user already exist"})
+}
+module.exports={getUserByname,createuser,follw,posst,getaa}
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {getUserByname,createuser}=require('../controllers/user.controllers')
+const {posst,getUserByname,createuser,follw,getaa}=require('../controllers/user.controllers')
 /* GET users listing. */
 router.get('/api/get-user/:username',getUserByname);
 router.post("/api/create-user/",createuser)
+router.post("/api/follow/:usernameA/:usernameB",follw)
+router.post("/api/create-post/:username",posst)
+router.post("/api/all-posts/:usernameA",getaa)
 
 module.exports = router;
